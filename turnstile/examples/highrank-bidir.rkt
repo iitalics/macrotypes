2de0899e89270beb82a5c1ab3e19c17d72562ad1
Milo Turner <iitalics@gmail.com>
2017-04-23 15:12:56 +0000

    Redoing exvar and context infrastructure

--- turnstile/examples/highrank-bidir.rkt
+++ turnstile/examples/highrank-bidir.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (require turnstile
-         (for-syntax racket syntax/parse)
+         (for-syntax racket syntax/parse
+                     rackunit)
          (for-meta 2 syntax/parse))
 
 
@@ -10,9 +11,13 @@
          unit)
 
 
+; types
 (define-base-type Unit)
 (define-type-constructor → #:arity = 2)
 (define-binding-type ∀ #:arity = 1 #:bvs = 1)
+
+; existential variables
+(define-type-constructor Exv #:arity = 1)
 
 (begin-for-syntax
   ;; syntax property utils
@@ -38,213 +43,97 @@
         [(~∀ (a) t) `(∀ (,(syntax-e #'a))
                         ,(->dat #'t))]
         [~Unit 'Unit]
+        [(~Exv _) `__]
         [x:id (syntax-e #'x)]))
     (format "~a" (->dat t))))
 
+
+
+;; existential variables (holes)
 (begin-for-syntax
-  (require rackunit)
-  ;; a context (Γ) is a syntax list of:
-  ;;   (TVAR a)
-  ;;   (EXVAR a)
-  ;;   (MARK a)
-  ;;   (a = τ)
-  ;;   (x : τ)
+  (define next-ex-uid (box 0))
 
-  ; this matches one of those by the head symbol,
-  ; and is used in the following pattern expanders
-  (define-syntax-class (Γ-var hd var)
-    (pattern (a b)
-             #:when (symbol=? (syntax-e #'a) hd)
-             #:when (bound-identifier=? #'b var)))
+  (define (generate-exv [id-stx #'a])
+    (define uid (unbox next-ex-uid))
+    (set-box! next-ex-uid (add1 uid))
+    (with-syntax ([uid/#%t (mk-type #`(quote #,uid))])
+      (eval-type (syntax/loc id-stx
+                   (Exv uid/#%t)))))
 
-  ; this matchs the constraint relation (a = τ)
-  (define-syntax-class (Γ-= var)
-    (pattern (a (~datum =) b)
-             #:when (bound-identifier=? #'a var)
-             #:with rhs #'b))
-
-  (define-syntax ~MARK
-    (pattern-expander
-     (syntax-parser [(_ v) #'(~var _ (Γ-var 'MARK v))])))
-  (define-syntax ~TYVAR
-    (pattern-expander
-     (syntax-parser [(_ v) #'(~var _ (Γ-var 'TVAR v))])))
-  (define-syntax ~EXVAR
-    (pattern-expander
-     (syntax-parser [(_ v) #'(~var _ (Γ-var 'EXVAR v))])))
-
-
-  ;; does T contain the variable v?
-  (define (fv-in? v T)
-    (syntax-parse T
-      [~Unit #f]
-      [(~→ t u) (or (fv-in? v #'t) (fv-in? v #'u))]
-      [(~∀ (a) t) (and (not (bound-identifier=? #'a v))
-                       (fv-in? v #'t))]
-      [x:id (bound-identifier=? #'x v)]))
-
-  (syntax-parse (eval-type #'(∀ (a) (∀ (b) (→ a Unit))))
-    [(~∀ (a) (~∀ (b) T))
-     (check-true (fv-in? #'a #'T))
-     (check-false (fv-in? #'b #'T))])
-
-
-  ;; apply all exvar constraints in a context to a type
-  (define (apply-ctx ctx T)
-    (define-values (xs τs)
-      (for/fold ([xs '()] [τs '()])
-                ([e (in-syntax ctx)])
-        (syntax-parse e
-          [(a (~datum =) rhs)
-           (values (cons #'a xs)
-                   (cons #'rhs τs))]
-          [_ (values xs τs)])))
-    (substs τs xs T))
-
-  (syntax-parse (eval-type #'(∀ (a) (∀ (b) (→ a Unit))))
-    [(~∀ (a) (~∀ (b) T))
-     #:with Γ #'[(a = Unit) (EXVAR u) (u = (→ Unit Unit))]
-     (check-equal? (type->string (apply-ctx #'Γ #'T))
-                   "(→ Unit Unit)")])
-
-  ;; is the type an exvar?
-  (define (exvar? a)
-    (and (identifier? a)
-         (prop a 'exvar?)))
-
-  ;; a monotype is a type without quantifications
-  (define (monotype? T)
-    (syntax-parse T
-      [~Unit #t]
-      [(~→ t u) (and (monotype? #'t)
-                     (monotype? #'u))]
-      [(~∀ (a) _) #f]
-      [x:id #t]))
-
-  (syntax-parse (eval-type #'(∀ (a) (→ a Unit)))
-    [(~and T (~∀ (a) t))
-     (check-true (monotype? #'t))
-     (check-false (monotype? #'T))])
-
-
-  ; algorithmic subtyping
-  (define (<: ctx t u)
-    (with-syntax ([Γ ctx])
-      (syntax-parse (list t u)
-        ; <:Unit
-        [(~Unit ~Unit)
-         #'Γ]
-
-        ; <:Var / <:Exvar
-        [(x:id y:id) #:when (bound-identifier=? #'x #'y)
-         #'Γ]
-
-        ; <:InstantiateL
-        [(^a:id A)
-         #:when (exvar? #'^a)
-         #:when (not (fv-in? #'^a #'A))
-         (</:= #'Γ #'^a #'A)]
-
-        ; <:InstantiateR
-        [(A ^a:id)
-         #:when (exvar? #'^a)
-         #:when (not (fv-in? #'^a #'A))
-         (</=: #'Γ #'A #'^a)]
-
-        ; <:-→
-        [((~→ A1 A2) (~→ B1 B2))
-         #:with Θ (<: #'Γ #'B1 #'A1)
-         #:with (A2- B2-) (apply-ctx #'Θ #'(A2 B2))
-         (<: #'Θ #'A2- #'B2-)]
-
-        ; <:∀L
-        [((~∀ (a) A) B)
-         #:with ^a (attach (generate-temporary #'a) 'exvar? #t)
-         #:with A- (subst #'^a #'a #'A)
-         #:with Γ- #'[(EXVAR ^a) (MARK ^a) . Γ]
-         #:with [_ ... (~MARK #'^a) . Δ] (<: #'Γ- #'A- #'B)
-         #'Δ]
-
-        ; <:∀R
-        [(A (~∀ (a) B))
-         #:with Γ- #'[(TYVAR a) . Γ]
-         #:with [_ ... (~TYVAR #'a) . Δ] (<: #'Γ- #'A #'B)
-         #'Δ]
-
-        [_ (raise 'no-subtype)])))
-
-  ; instantiation (left)
-  (define (</:= ctx var T)
-    (with-syntax ([Γ ctx] [^a var])
-      (syntax-parse T
-        ; InstLSolve
-        [τ
-         #:when (monotype? #'τ)
-         #:with [γ- ... (~EXVAR #'^a) γ ...] #'Γ
-         #'[γ- ... (^a = τ) γ ...]]
-
-        ; InstLReach
-        [^b:id
-         #:when (exvar? #'^b)
-         #:with [γR ... (~EXVAR #'^b) γM ... (~EXVAR #'^a) γL ...] #'Γ
-         #'[γR ... (^b = ^a) γM ... (EXVAR ^a) yL ...]]
-
-        ; InstLArr
-        [(~→ A1 A2)
-         #:with [γ- ... (~EXVAR #'^a) γ ...] #'Γ
-         #:with (tmp1 tmp2) (generate-temporaries #'(^a ^a))
-         #:with (~∀ (^a1) (~∀ (^a2) arrow))
-         (eval-type #'(∀ (tmp1) (∀ (tmp2) (→ tmp1 tmp2))))
-         #:with Γ- #'[γ- ... (^a = arrow) (EXVAR ^a1) (EXVAR ^a2) γ ...]
-         #:with Θ (</=: #'Γ- #'A1 #'^a1)
-         #:with (A2-) (apply-ctx #'Θ #'(A2))
-         (</:= #'Θ #'^a2 #'A2-)]
-
-        ; InstLAllR
-        [(∀ (b) B)
-         #:with Γ- #'[b . Γ]
-         #:with [_ ... (TYVAR #'b) . Δ] (</:= #'Γ- #'^a #'B)
-         #'Δ]
-
-        [_ (raise 'no-instantiate)])))
-
-  ; instantiation (right)
-  (define (</=: ctx T var)
-    (with-syntax ([Γ ctx] [^a var])
-      (syntax-parse T
-        ; InstRSolve
-        [τ
-         #:when (monotype? #'τ)
-         #:with [γ- ... (~EXVAR #'^a) γ ...] #'Γ
-         #'[γ- ... (^a = τ) γ ...]]
-
-        ; InstRReach
-        [^b:id
-         #:when (exvar? #'^b)
-         #:with [γR ... (~EXVAR #'^b) γM ... (~EXVAR #'^a) γL ...] #'Γ
-         #'[γR ... (^b = ^a) γM ... (EXVAR ^a) yL ...]]
-
-        ; InstRArr
-        [(~→ A1 A2)
-         #:with [γ- ... (~EXVAR #'^a) γ ...] #'Γ
-         #:with (tmp1 tmp2) (generate-temporaries #'(^a ^a))
-         #:with (~∀ (^a1) (~∀ (^a2) arrow))
-         (eval-type #'(∀ (tmp1) (∀ (tmp2) (→ tmp1 tmp2))))
-         #:with Γ- #'[γ- ... (^a = arrow) (EXVAR ^a1) (EXVAR ^a2) γ ...]
-         #:with Θ (</:= #'Γ- #'^a1 #'A1)
-         #:with (A2-) (apply-ctx #'Θ #'(A2))
-         (</=: #'Θ #'A2- #'^a2)]
-
-        ; InstRAllL
-        [(∀ (b) B)
-         (raise 'meh)]
-
-        [_ (raise 'no-instantiate)])))
+  (define (Exv=? e1 e2)
+    (syntax-parse (list e1 e2)
+      #:literals (quote)
+      [((~Exv 'a) (~Exv 'b))
+       (equal? (syntax-e #'a) (syntax-e #'b))]
+      [_ #f]))
 
   )
 
 
+;; contexts
+(begin-for-syntax
 
+  (define (exv/id=? a b)
+    (cond
+      [(and (identifier? a) (identifier? b))
+       (bound-identifier=? a b)]
+      [(and (Exv? a) (Exv? b))
+       (Exv=? a b)]
+      [else
+       (equal? a b)]))
+
+  ; split a context in half by the specified key and second elem
+  (define (ctx-split-one ctx key snd)
+    (define-values (L R)
+      (splitf-at ctx
+                 (negate (lambda (e)
+                           (and (symbol=? (first e) key)
+                                (exv/id=? snd (second e)))))))
+    (list L (rest R)))
+
+  (let ([e1 (generate-exv)]
+        [e2 (generate-exv)])
+    (check-equal? (ctx-split-one (list (list '▹ e1)
+                                       (list ': #'x e2)
+                                       (list '▹ e2))
+                                 ': #'x)
+                  (list (list (list '▹ e1))
+                        (list (list '▹ e2)))))
+
+
+  ; split a context into parts by specifying multiple keys and elems
+  (define (ctx-split ctx . keys)
+    (define (evens lst)
+      (if (null? lst) '()
+          (cons (car lst) (odds (cdr lst)))))
+    (define (odds lst)
+      (if (null? lst) '()
+          (evens (cdr lst))))
+    (foldr (lambda (key snd ctxs)
+             (append (ctx-split-one (first ctxs) key snd)
+                     (rest ctxs)))
+           (list ctx)
+           (evens keys)
+           (odds keys)))
+
+  (let ([e1 (generate-exv)]
+        [e2 (generate-exv)]
+        [e3 (generate-exv)])
+    (check-equal? (ctx-split (list (list '▹ e1)
+                                   (list ': 'x e2)
+                                   (list 't (eval-type e2))
+                                   (list ': 'y e3)
+                                   (list '▹ e3))
+                             't e2
+                             ': 'y)
+                  (list (list (list '▹ e1)
+                              (list ': 'x e2))
+                        (list)
+                        (list (list '▹ e3)))))
+)
+
+
+;; inference helpers
 (begin-for-syntax
   ;; kinds of exceptions
   (struct exn:no-checking exn:fail:syntax ())
@@ -282,7 +171,6 @@
             (or (prop e- 'ctx⇒)
                 (and (displayln "warning: no output context")
                      ctx)))))
-
 
   (define (typed-transformer out-stx τ)
     (make-set!-transformer
