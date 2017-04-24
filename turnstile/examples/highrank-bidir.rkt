12afa5ba68ecc81951d3524435b66c85d10eca86
Milo Turner <iitalics@gmail.com>
2017-04-24 02:27:01 +0000

    rewrote simpler unit rule

--- turnstile/examples/highrank-bidir.rkt
+++ turnstile/examples/highrank-bidir.rkt
@@ -6,8 +6,7 @@
 
 
 (provide (rename-out [mod-begin #%module-begin]
-                     [top #%top-interaction]
-                     [do-nothing/τ um])
+                     [top #%top-interaction])
          unit)
 
 
@@ -448,81 +447,22 @@
      (lambda ()
        (subtype (list)
                 (eval-type #`(∀ (X) (→ X X)))
-                (eval-type #`(→ Unit Unit)))))
-
-    (displayln "\nalpha-equiv test.")
-    (displayln (subtype '()
-                        (eval-type #'(∀ (X) X))
-                        (eval-type #'(∀ (Y) Y)))))
-
+                (eval-type #`(→ Unit Unit))))))
 
   )
 
 
 ;; inference helpers
 (begin-for-syntax
-  ;; kinds of exceptions
-  (struct exn:no-checking exn:fail:syntax ())
-  (struct exn:no-synthesis exn:fail:syntax ())
-  (struct exn:no-apply exn:fail:syntax ())
 
-  (define (raise-no-checking s)
-    (raise (exn:no-checking "no checking rule"
-                            (current-continuation-marks)
-                            (list s))))
+  (define (ctx-of s [default '()])
+    (or (syntax-property s 'Γ)
+        default))
 
-  (define (raise-no-apply s)
-    (raise (exn:no-apply "expression cannot be applied"
-                         (current-continuation-marks)
-                         (list s))))
+  (define (set-ctx-of s ctx)
+    (syntax-property s 'Γ ctx))
 
-  (define (infer e #:Γ ctx)
-    (let* ([e* (attach e
-                       'ctx⇐ ctx
-                       'ty⇐ #f)]
-           [e- (local-expand e* 'expression '())])
-      (list e-
-            (or (prop e- 'ty⇒)
-                (raise-syntax-error #f "no type inferred!" e))
-            (or (prop e- 'ctx⇒)
-                (and (displayln "warning: no output context")
-                     ctx)))))
-
-  (define (check e τ #:Γ ctx)
-    (let* ([e* (attach e
-                       'ctx⇐ ctx
-                       'ty⇐ τ)]
-           [e- (local-expand e* 'expression '())])
-      (list e-
-            (or (prop e- 'ctx⇒)
-                (and (displayln "warning: no output context")
-                     ctx)))))
-
-  (define (typed-transformer out-stx τ)
-    (make-set!-transformer
-     (syntax-parser
-       [((~literal set!) a ...)
-        (raise-syntax-error #f "cannot set! typed syntax" this-syntax)]
-       [(a . b)
-        (with-syntax ([ap (datum->syntax this-syntax '#%app)])
-          #'(ap a . b))]
-
-       [_ #:when (prop this-syntax 'ty⇐)
-          (raise-no-checking this-syntax)]
-
-       [_ ; infer
-        #:with Γ (prop this-syntax 'ctx⇐)
-          (attach out-stx
-                  'ctx⇒ #'Γ
-                  'ty⇒ (eval-type τ))])))
   )
-
-
-(define (do-nothing x) (cons 'um x))
-(define-syntax do-nothing/τ
-  (typed-transformer #'do-nothing
-                     #'(→ Unit Unit)))
-
 
 
 
@@ -531,30 +471,22 @@
     [(_ ...) #'(#%module-begin)]))
 
 
-
-(define-syntax top
-  (syntax-parser
-    [(_ . e)
-     #:with (e- τ Γ) (infer #'e #:Γ '())
-     #:with τ/s (type->str #'τ)
-     #'(printf "~a : ~a\nΓ = ~s\n"
-               e-
-               'τ/s
-               'Γ)]))
+(define-typed-syntax top
+  [(_ . e) ≫
+   [⊢ e ≫ e- ⇒ T]
+   #:with T- (ctx-subst (ctx-of #'e-) #'T)
+   --------
+   [≻ (printf "~a : ~a\n"
+              e-
+              '#,(type->string #'T-))]])
 
 
+(define-typed-syntax unit
+  [(_) ≫
+   --------
+   [⊢ '() ⇒ Unit]]
 
-(define-syntax unit
-  (syntax-parser
-    [(_) ; check
-     #:with Γ (prop this-syntax 'ctx⇐)
-     #:attr in (prop this-syntax 'ty⇐) #:when (attribute in)
-     #:fail-unless (Unit? #'in) "expected Unit type"
-     (attach #'null
-             'ctx⇒ #'Γ)]
-
-    [(_) ; infer
-     #:with Γ (prop this-syntax 'ctx⇐)
-     (attach #'null
-             'ty⇒ (eval-type #'Unit)
-             'ctx⇒ #'Γ)]))
+  [(_) ⇐ T ≫
+   #:when (Unit? #'T)
+   --------
+   [⊢ '()]])
