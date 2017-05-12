f86e9c867cce418f1bac415f78463b98691e3ff8
Milo Turner <iitalics@gmail.com>
2017-05-12 10:34:38 +0000

    context mutation functions

---
+++ turnstile/examples/contexty/context.rkt
@@ -0,0 +1,57 @@
+#lang racket
+
+(begin-for-syntax
+  (require racket/base
+           racket/match
+           (only-in racket/function negate)
+           (only-in racket/list takef dropf)
+           syntax/parse
+           (for-syntax racket/base
+                       syntax/parse))
+
+
+  (define the-context (make-parameter '()))
+
+  (define (set-context! to)
+    [the-context to])
+
+  (define (context-before-pred p)
+    (match (dropf (the-context) (negate p))
+      [(cons _ xs) xs]
+      [y y]))
+
+  (define (context-after-pred p)
+    (takef (the-context) (negate p)))
+
+
+
+  (define-syntax context-before
+    (syntax-parser
+      [(_ patn)
+       #'(context-before-pred (syntax-parser [patn #t] [_ #f]))]))
+
+  (define-syntax context-after
+    (syntax-parser
+      [(_ patn)
+       #'(context-after-pred (syntax-parser [patn #t] [_ #f]))]))
+
+  (define-syntax context-push!
+    (syntax-parser
+      [(_ expr ...)
+       #'(set-context! (append (reverse (list ((current-type-eval) expr) ...))
+                               (the-context)))]))
+
+  (define-syntax context-replace!
+    (syntax-parser
+      [(_ patn expr ...)
+       #'(let ([p (syntax-parser [patn #t] [_ #f])])
+           (set-context! (append (context-after-pred p)
+                                 (reverse (list ((current-type-eval) expr) ...))
+                                 (context-before-pred p))))]))
+
+  (define-syntax context-pop-until!
+    (syntax-parser
+      [(_ patn)
+       #'(set-context! (context-before patn))]))
+
+  )
