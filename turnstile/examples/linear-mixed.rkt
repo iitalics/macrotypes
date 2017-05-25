c02e7050098fe2112ad059307300d3ca4e3ef1c8
Milo Turner <iitalics@gmail.com>
2017-05-25 14:52:06 -0400

    fixed unrestricted? and ->linear (forgot ~ in pattern)

--- turnstile/examples/linear-mixed.rkt
+++ turnstile/examples/linear-mixed.rkt
@@ -30,14 +30,14 @@
   (define unrestricted?
     (syntax-parser
       [(~or ~Unit ~Int ~Bool ~Str) #t]
-      [(-> τ ...) (andmap unrestricted? (syntax-e #'(τ ...)))]
-      [(× τ ...)  (andmap unrestricted? (syntax-e #'(τ ...)))]
+      [(~-> τ ...) (andmap unrestricted? (syntax-e #'(τ ...)))]
+      [(~× τ ...)  (andmap unrestricted? (syntax-e #'(τ ...)))]
       [_ #f]))
 
   (define (->linear ty)
     (syntax-parse ty
-      [(-> τ ...) ((current-type-eval) (syntax/loc ty (-o τ ...)))]
-      [(× τ ...)  ((current-type-eval) (syntax/loc ty (⊗ τ ...)))]
+      [(~-> τ ...) ((current-type-eval) (syntax/loc ty (-o τ ...)))]
+      [(~× τ ...)  ((current-type-eval) (syntax/loc ty (⊗ τ ...)))]
       [_ ty]))
 
 
