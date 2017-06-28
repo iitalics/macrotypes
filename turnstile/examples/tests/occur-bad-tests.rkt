a0b31997c90be729d71637cacf9ef480368655c1
Milo Turner <iitalics@gmail.com>
2017-06-28 00:02:09 -0400

    more tests

--- turnstile/examples/tests/occur-bad-tests.rkt
+++ turnstile/examples/tests/occur-bad-tests.rkt
@@ -9,3 +9,16 @@
                 [(integer? x) x]
                 [else 0]))
             : Int)
+
+(check-type (let ([x (read)])
+              (cond
+                [(string? x) x]
+                [(integer? x) "<number>"]
+                [else (error "invalid input")]))
+            : String)
+
+(typecheck-fail (let ([x (read)])
+                  (cond
+                    [#f  "some string"]
+                    [else x]))
+                #:with-msg "expected String, given Any")
