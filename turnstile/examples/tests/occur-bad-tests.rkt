759ba6dd698dd86b8c003b1804434512cdfa9917
Milo Turner <iitalics@gmail.com>
2017-06-30 13:22:16 -0400

    additional test for occur-bad

--- turnstile/examples/tests/occur-bad-tests.rkt
+++ turnstile/examples/tests/occur-bad-tests.rkt
@@ -22,3 +22,15 @@
                     [#f  "some string"]
                     [else x]))
                 #:with-msg "expected String, given Any")
+
+; i am worried about free-identifier=? causing problems
+; which would be exemplified here (the latter bound 'x'
+; thinking that the deduction (integer? x) applies to it
+; as well).
+; but this does fail as it's supposed to, so we're ok.
+(typecheck-fail (let ([x (read)])
+                  (cond
+                    [(integer? x)
+                     (let ([x "hi"])
+                       (if #t x 0))]
+                    [else 1])))
